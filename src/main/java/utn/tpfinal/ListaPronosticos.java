9cd93339fcc86dca28d03ce62e044f372626d92b
LEANDRO GABRIEL DEQUINO <ldequino1973@gmail.com>
2023-04-19 18:43:59 -0300

    Mas cambios

--- src/main/java/utn/tpfinal/ListaPronosticos.java
+++ src/main/java/utn/tpfinal/ListaPronosticos.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class ListaPronosticos {
